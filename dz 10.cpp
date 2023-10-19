7729f17d9fb00a571499eddcd1228762a9c18971
Stryzhakov Bogdan <bogdan24086@gmail.com>
2023-10-19 03:13:44 +0300

    Добавьте файлы проекта.

---
+++ dz 10.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+#include "Header.h"
+using namespace std;
+int main() {
+    //task 1
+    /*int arr[] = { 5, 2, 8, 1, 9, 3, 6, 4, 7 };
+    int size = sizeof(arr) / sizeof(arr[0]);
+    sort(arr, size, moreNumber);
+    cout << "Сортировка по возрастанию: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    sort(arr, size, lessNumber);
+    cout << "Сортировка по убыванию: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    sort(arr, size, evenNumber);
+    cout << "Сортировка четных в начале, нечетных в конце: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;*/
+
+    //task2
+   /* int arr[] = { -3, 5, 2, 8, -1, 9, 3, 6, 4, 7 };
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int lastPositive = findLast_if(arr, size, isPositive);
+    int lastNegative = findLast_if(arr, size, isNegative);
+    int lastEven = findLast_if(arr, size, isEven);
+    int lastOdd = findLast_if(arr, size, isOdd);
+    if (lastPositive != -1) {
+        cout << "Индекс последнего положительного элемента: " << lastPositive << endl;
+    }
+    else {
+        cout << "Положительных элементов не найдено." << endl;
+    }
+    if (lastNegative != -1) {
+        cout << "Индекс последнего отрицательного элемента: " << lastNegative << endl;
+    }
+    else {
+        cout << "Отрицательных элементов не найдено." << endl;
+    }
+    if (lastEven != -1) {
+        cout << "Индекс последнего четного элемента: " << lastEven << endl;
+    }
+    else {
+        cout << "Четных элементов не найдено." << endl;
+    }
+    if (lastOdd != -1) {
+        cout << "Индекс последнего нечетного элемента: " << lastOdd << endl;
+    }
+    else {
+        cout << "Нечетных элементов не найдено." << endl;
+    }*/
+    return 0;
+}
